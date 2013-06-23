d1bf2455d92300c4d9290a3ba8d1e0ceae933ed5
Toke Høiland-Jørgensen <toke@toke.dk>
2013-06-23 19:03:59 +0200

    Add CSV formatter.

---
+++ src/csv_formatter.c
@@ -0,0 +1,36 @@
+/**
+ * csv_formatter.c
+ *
+ * Toke Høiland-Jørgensen
+ * 2013-06-23
+ */
+
+#include "formatter.h"
+
+int csv_format(struct formatter *fmt, struct recordset *rset)
+{
+	struct record *r;
+	static char first = 1;
+	if(first) {
+		first = 0;
+		for_each_record(rset, r)
+			if(!r->composite) {
+				fputs(r->name, fmt->f);
+				if(r->next)
+					fputs(",", fmt->f);
+			}
+		fprintf(fmt->f, "\n");
+	}
+	for_each_record(rset, r)
+		if(!r->composite) {
+			fputs(r->value, fmt->f);
+			if(r->next)
+				fputs(",", fmt->f);
+		}
+	fprintf(fmt->f, "\n");
+}
+
+struct formatter csv_formatter = {
+	.id = "csv",
+	.format = csv_format,
+};
