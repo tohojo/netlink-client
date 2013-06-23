30ae709fb3bd8d7d87e9f2d8a6e4c8a6b8d52737
Toke Høiland-Jørgensen <toke@toke.dk>
2013-06-24 00:20:24 +0200

    Do string comparisons on header instead of length comparisons.

--- src/csv_formatter.c
+++ src/csv_formatter.c
@@ -5,30 +5,43 @@
  * 2013-06-23
  */
 
+#include <string.h>
+
 #include "formatter.h"
+
+int create_header(char *buf, unsigned int maxlen, struct recordset *rset)
+{
+	struct record *r;
+	unsigned int pos = 0;
+	for_each_record(r, rset) {
+		pos += snprintf(buf+pos, maxlen-pos, "%s", r->name);
+		if(r->next)
+			pos += snprintf(buf+pos, maxlen-pos, ",");
+		if(pos >= maxlen)
+			return -1;
+	}
+	return 0;
+}
 
 int csv_format(struct formatter *fmt, struct recordset *rset)
 {
 	struct record *r;
-	static char first = 1;
-	if(first) {
-		first = 0;
-		for_each_record(r, rset)
-			if(!r->composite) {
-				fputs(r->name, fmt->f);
-				if(r->next)
-					fputs(",", fmt->f);
-			}
-		fprintf(fmt->f, "\n");
+	static char header[255];
+	char buf[255];
+	create_header(buf, sizeof(buf), rset);
+	if(strcmp(header, buf) != 0) {
+		strncpy(header, buf, sizeof(header));
+		fputs(header, fmt->f);
+		fputs("\n", fmt->f);
 	}
-	for_each_record(r, rset)
-		if(!r->composite) {
-			fputs(r->value, fmt->f);
-			if(r->next)
-				fputs(",", fmt->f);
-		}
-	fprintf(fmt->f, "\n");
+	for_each_record(r, rset){
+		fputs(r->value, fmt->f);
+		if(r->next)
+			fputs(",", fmt->f);
+	}
+	fputs("\n", fmt->f);
 }
+
 
 struct formatter csv_formatter = {
 	.id = "csv",
