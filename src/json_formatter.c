3882858acf63c1557ad98b154a3f73329599876b
Toke Høiland-Jørgensen <toke@toke.dk>
2013-06-25 00:34:23 +0200

    Add recursive call for record set type record.

--- src/json_formatter.c
+++ src/json_formatter.c
@@ -53,6 +53,9 @@ static int json_format(struct formatter *fmt, struct recordset *rset)
 		case RECORD_TYPE_HEX:
 			yajl_gen_integer(priv->yajl, r->value_uint);
 			break;
+		case RECORD_TYPE_RSET:
+			json_format(fmt, r->value_rset);
+			break;
 		default:
 			break;
 		}
